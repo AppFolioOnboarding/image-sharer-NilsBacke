ebd70d889275212353d3bff02f6a27f350eeab7c
Nils Backe <nils.backe@appfolio.com>
2020-01-08 16:39:31 -0800

    nb - dont destroy images in tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -4,21 +4,21 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   def test_assigns_image_in_new
     image = Image.new
     get new_image_path
+    assert_response :ok
     assert_nil assigns(:image).url
     assert_nil image.url
-    image.destroy!
   end
 
   def test_assigns_image_in_create
     image = Image.new(url: 'https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg')
     post images_path, params: { image: { url: 'https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg' } }
     assert_equal assigns(:image).url, image.url
-    image.destroy!
   end
 
   def test_assigns_image_in_show
     image = Image.create(url: 'https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg')
     get image_path(image.id)
+    assert_response :ok
     assert_equal assigns(:image).id, image.id
     image.destroy!
   end
